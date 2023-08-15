feffe36affa99888a9e8eb623bfbb1ab84958f89
Diiegote <diego.elprado22@gmail.com>
2023-08-15 18:19:10 -0300

    database

--- api/index.js
+++ api/index.js
@@ -20,11 +20,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config();
-const port=process.env.PORT || 3001;
+const port=process.env.PORT || 3000;
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(3001, () => {
+  server.listen(3000, () => {
     console.log(`%s listening at ${port}` ); // eslint-disable-line no-console
   });
 });
